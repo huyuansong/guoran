9243ec760cd5572caf80f5edfeb6b0c81817633f
魏梦 <2751338876@qq.com>
2023-02-23 13:35:07 +0800

    修改了玄学问题
    Signed-off-by: wei <2751338876@qq.com>

--- src/main/java/com/guoran/server/test/repository/BalanceRepostiory.java
+++ src/main/java/com/guoran/server/test/repository/BalanceRepostiory.java
@@ -1,12 +1,12 @@
 package com.guoran.server.test.repository;
 
 
-import com.guoran.server.test.model.balance;
+import com.guoran.server.test.model.Balance;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
-public interface balancerepostiory {
-	List<balance> findAll();
+public interface BalanceRepostiory {
+	List<Balance> findAll();
 }
