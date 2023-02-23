1249ebdf46c9339935cdba13c1cbe9d895ea91ad
bch <869545607@qq.com>
2023-02-23 15:26:47 +0800

    修改了描述和开发文档
    Signed-off-by: bch <869545607@qq.com>

--- src/main/java/com/guoran/server/bu/equipment/service/Impl/PurifiedBottleRunInfoServiceImpl.java
+++ src/main/java/com/guoran/server/bu/equipment/service/Impl/PurifiedBottleRunInfoServiceImpl.java
@@ -14,22 +14,20 @@ import com.guoran.server.common.search.PageQuery;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Date;
-
 /**
  * w
  * 2023/1/25  15:20
  * 01-pro
  **/
 public class PurifiedBottleRunInfoServiceImpl implements PurifiedBottleRunInfoService {
-    @Autowired
-    JwtUserUtil jwtUserUtil;
+    //    @Autowired
+//    JwtUserUtil jwtUserUtil;
     @Autowired
     PurifiedBottleRunInfoRepository purifiedBottleRunInfoRepository;
-    @Autowired
-    ProductRepository productRepository;
-    @Autowired
-    DepartmentRepository departmentRepository;
+//    @Autowired
+//    ProductRepository productRepository;
+//    @Autowired
+//    DepartmentRepository departmentRepository;
 
     /**
      * 根据id获取
@@ -53,19 +51,19 @@ public class PurifiedBottleRunInfoServiceImpl implements PurifiedBottleRunInfoSe
      */
     @Override
     public String createEntry(PurifiedBottleRunInfoVM purifiedBottleRunInfoVM) {
-        //获取部门名
-        DepartmentEntity departmentEntity = departmentRepository.findBycode(purifiedBottleRunInfoVM.getDepartId());
-        //获取商品名
-        ProductEntity productEntity = productRepository.finByCode(purifiedBottleRunInfoVM.getProdectCode());
-        PurifiedBottleRunInfoEntity purifiedBottleRunInfoEntity = new PurifiedBottleRunInfoEntity();
-        BeanUtils.copyProperties(purifiedBottleRunInfoVM, purifiedBottleRunInfoEntity);
-        purifiedBottleRunInfoEntity.setDepartName(departmentEntity.getDepartmentName());
-        purifiedBottleRunInfoEntity.setProdectName(productEntity.getProductName());
-        purifiedBottleRunInfoEntity.setCreateBy(jwtUserUtil.getUserName());
-        purifiedBottleRunInfoEntity.setCreateTime(new Date());
-//        purifiedBottleRunInfoEntity.setUpdateTime(new Date());
-//        purifiedBottleRunInfoEntity.setNoteTakerTime();
-        purifiedBottleRunInfoRepository.insert(purifiedBottleRunInfoEntity);
+//        //获取部门名
+//        DepartmentEntity departmentEntity = departmentRepository.findBycode(purifiedBottleRunInfoVM.getDepartId());
+//        //获取商品名
+//        ProductEntity productEntity = productRepository.finByCode(purifiedBottleRunInfoVM.getProdectCode());
+//        PurifiedBottleRunInfoEntity purifiedBottleRunInfoEntity = new PurifiedBottleRunInfoEntity();
+//        BeanUtils.copyProperties(purifiedBottleRunInfoVM, purifiedBottleRunInfoEntity);
+//        purifiedBottleRunInfoEntity.setDepartName(departmentEntity.getDepartmentName());
+//        purifiedBottleRunInfoEntity.setProdectName(productEntity.getProductName());
+//        purifiedBottleRunInfoEntity.setCreateBy(jwtUserUtil.getUserName());
+//        purifiedBottleRunInfoEntity.setCreateTime(new Date());
+////        purifiedBottleRunInfoEntity.setUpdateTime(new Date());
+////        purifiedBottleRunInfoEntity.setNoteTakerTime();
+//        purifiedBottleRunInfoRepository.insert(purifiedBottleRunInfoEntity);
         return null;
     }
 
@@ -77,18 +75,18 @@ public class PurifiedBottleRunInfoServiceImpl implements PurifiedBottleRunInfoSe
      */
     @Override
     public String updateEntry(PurifiedBottleRunInfoVM purifiedBottleRunInfoVM) throws ServiceException {
-        //获取部门名
-        DepartmentEntity departmentEntity = departmentRepository.findBycode(purifiedBottleRunInfoVM.getDepartId());
-        //获取商品名
-        ProductEntity productEntity = productRepository.finByCode(purifiedBottleRunInfoVM.getProdectCode());
-        PurifiedBottleRunInfoEntity purifiedBottleRunInfoEntity = purifiedBottleRunInfoRepository.findById(purifiedBottleRunInfoVM.getId());
-        purifiedBottleRunInfoEntity.failWhenConcurrencyViolation(purifiedBottleRunInfoVM.getConcurrencyVersion());
-        BeanUtils.copyProperties(purifiedBottleRunInfoVM, purifiedBottleRunInfoEntity);
-        purifiedBottleRunInfoEntity.setDepartName(departmentEntity.getDepartmentName());
-        purifiedBottleRunInfoEntity.setProdectName(productEntity.getProductName());
-        purifiedBottleRunInfoEntity.setUpdateBy(jwtUserUtil.getUserName());
-        purifiedBottleRunInfoEntity.setUpdateTime(new Date());
-        purifiedBottleRunInfoRepository.update(purifiedBottleRunInfoEntity);
+//        //获取部门名
+//        DepartmentEntity departmentEntity = departmentRepository.findBycode(purifiedBottleRunInfoVM.getDepartId());
+//        //获取商品名
+//        ProductEntity productEntity = productRepository.finByCode(purifiedBottleRunInfoVM.getProdectCode());
+//        PurifiedBottleRunInfoEntity purifiedBottleRunInfoEntity = purifiedBottleRunInfoRepository.findById(purifiedBottleRunInfoVM.getId());
+//        purifiedBottleRunInfoEntity.failWhenConcurrencyViolation(purifiedBottleRunInfoVM.getConcurrencyVersion());
+//        BeanUtils.copyProperties(purifiedBottleRunInfoVM, purifiedBottleRunInfoEntity);
+//        purifiedBottleRunInfoEntity.setDepartName(departmentEntity.getDepartmentName());
+//        purifiedBottleRunInfoEntity.setProdectName(productEntity.getProductName());
+//        purifiedBottleRunInfoEntity.setUpdateBy(jwtUserUtil.getUserName());
+//        purifiedBottleRunInfoEntity.setUpdateTime(new Date());
+//        purifiedBottleRunInfoRepository.update(purifiedBottleRunInfoEntity);
         return null;
     }
 
