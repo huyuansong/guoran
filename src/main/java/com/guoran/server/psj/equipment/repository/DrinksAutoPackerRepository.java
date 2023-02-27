9a73ec456efe9198e99c9c1a5c36a85bd51d19f8
daofengbo <1178825944@qq.com>
2023-02-27 09:45:24 +0800

    升级
    Signed-off-by: daofengbo <1178825944@qq.com>

--- src/main/java/com/guoran/server/psj/equipment/repository/DrinksAutoPackerRepository.java
+++ src/main/java/com/guoran/server/psj/equipment/repository/DrinksAutoPackerRepository.java
@@ -4,13 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.pagehelper.Page;
 import com.guoran.server.psj.equipment.model.DrinksAutoPackerEntity;
 import com.guoran.server.psj.equipment.model.vto.DrinksAutoPackerVM;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 /**
  * 设备管理-设备运行记录-饮料车间-自动包装机运行记录数据访问层
  */
-@Mapper
 public interface DrinksAutoPackerRepository extends BaseMapper<DrinksAutoPackerEntity> {
 
     /**
@@ -19,7 +16,6 @@ public interface DrinksAutoPackerRepository extends BaseMapper<DrinksAutoPackerE
      * @param where
      * @return
      */
-    @Select("select * from drinks_auto_packer where 1=1 ${where}")
     Page<DrinksAutoPackerVM> findEntrysByPage(String where);
 }
 
--- src/main/java/com/guoran/server/psj/equipment/repository/DrinksCansFillingRepository.java
+++ src/main/java/com/guoran/server/psj/equipment/repository/DrinksCansFillingRepository.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.pagehelper.Page;
 import com.guoran.server.psj.equipment.model.DrinksCansFillingEntity;
 import com.guoran.server.psj.equipment.model.vto.DrinksCansFillingVM;
-import org.apache.ibatis.annotations.Select;
 
 public interface DrinksCansFillingRepository extends BaseMapper<DrinksCansFillingEntity> {
 
@@ -14,7 +13,6 @@ public interface DrinksCansFillingRepository extends BaseMapper<DrinksCansFillin
      * @param where
      * @return
      */
-    @Select("select * from drinks_cans_filling where 1=1 ${where}")
     Page<DrinksCansFillingVM> findEntrysByPage(String where);
 
 }
